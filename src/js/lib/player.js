33b7d2eac4420b9372b45c8bdc96425df0b834d6
CraigChilds94 <childscraig17@gmail.com>
2015-06-25 13:32:55 +0100

    Add a wall in and detect collision on it

--- src/js/lib/player.js
+++ src/js/lib/player.js
@@ -3,6 +3,8 @@ var Player = (function(world, position) {
     // Load in a player image
     var sprite = new world.PIXI.Sprite.fromImage('http://placekitten.com/g/150/150');
 
+    var hitWall = false;
+
     // deltas
     var delta = {
         x: 0,
@@ -15,7 +17,7 @@ var Player = (function(world, position) {
     // Handle updating the player
     function update()
     {
-        if(inBounds()) {
+        if(!world.player.hitWall && inBounds()) {
             sprite.position.x += delta.x;
             sprite.position.y += delta.y;
         }
@@ -36,7 +38,8 @@ var Player = (function(world, position) {
         sprite: sprite,
         speed: 5,
         delta: delta,
-        update: update
+        update: update,
+        hitWall: hitWall
     };
 
 });
